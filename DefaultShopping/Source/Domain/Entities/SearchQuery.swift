02c95270033888ab8b22b011072f7889be4a0f36
Kim junhwan <tazanboy1@naver.com>
2023-09-08 18:59:35 +0900

    [feat] 검색 요청 쿼리 구현

---
+++ DefaultShopping/Source/Domain/Entities/SearchQuery.swift
@@ -0,0 +1,12 @@
+//
+//  SearchQuery.swift
+//  DefaultShopping
+//
+//  Created by JunHwan Kim on 2023/09/08.
+//
+
+struct SearchQuery {
+    let query: String
+    let start: Int
+    let sort: String
+}
