aa59cef4040628a6b349b3a5b43882d670eba5d3
Kim junhwan <tazanboy1@naver.com>
2023-09-09 10:26:08 +0900

    [feat] NetworkConfiguration 구현

---
+++ DefaultShopping/Source/Network/NetworkConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkConfiguration.swift
+//  DefaultShopping
+//
+//  Created by JunHwan Kim on 2023/09/09.
+//
+
+import Foundation
+
+struct NetworkConfiguration {
+    var baseURL: URL
+    var header: [String:String]
+    var queryParameter: [String:String]
+}
