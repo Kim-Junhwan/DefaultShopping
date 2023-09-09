d01c9ac7bd5aaf52e3bd23f3a647992e1ba52e67
Kim junhwan <tazanboy1@naver.com>
2023-09-09 20:55:35 +0900

    [feat] 검색 Repository 구현

---
+++ DefaultShopping/Source/Data/Repositories/DefaultShoppingRepository.swift
@@ -0,0 +1,32 @@
+//
+//  DegaultShoppingRepository.swift
+//  DefaultShopping
+//
+//  Created by JunHwan Kim on 2023/09/09.
+//
+
+import Foundation
+
+final class DefaultShoppingRepository {
+    private let dataTransferService: DataTransferService
+    
+    init(dataTransferService: DataTransferService) {
+        self.dataTransferService = dataTransferService
+    }
+}
+
+extension DefaultShoppingRepository: ShoppingRepository {
+    
+    func searchShoppingList(query: SearchQuery, completion: @escaping (Result<ProductPage, Error>) -> Void) {
+        let endpoint = APIEndpoints.searchProduct(requestDTO: .init(page: query.page, query: query.query, sort: query.sort))
+        dataTransferService.request(endpoint: endpoint) { result in
+            switch result {
+            case .success(let success):
+                print(success.toDomain())
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
+    
+}
