21b34855712b66f80f176ec0f9384c26b3f3b8f5
Kim junhwan <tazanboy1@naver.com>
2023-09-11 09:37:06 +0900

    [feat] BaseView navigationcontroller 타이틀 색상 지정

--- DefaultShopping/Source/Presentation/Base/BaseViewController.swift
+++ DefaultShopping/Source/Presentation/Base/BaseViewController.swift
@@ -13,6 +13,13 @@ class BaseViewController: UIViewController {
         super.viewDidLoad()
         configureView()
         setConstraints()
+        setNavigationController()
+    }
+    
+    private func setNavigationController() {
+        let appearence = UINavigationBarAppearance()
+        appearence.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationItem.standardAppearance = appearence
     }
     
     func configureView() {
@@ -20,7 +27,6 @@ class BaseViewController: UIViewController {
     }
     
     func setConstraints() {
-        
     }
 
 }
