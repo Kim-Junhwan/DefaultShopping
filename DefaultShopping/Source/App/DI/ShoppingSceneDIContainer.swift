84a1ed80d98e45549b9b7b51299bce8c85a457df
Kim junhwan <tazanboy1@naver.com>
2023-09-11 22:35:32 +0900

    [feat] DIContainer에 의존성 추가

--- DefaultShopping/Source/App/DI/ShoppingSceneDIContainer.swift
+++ DefaultShopping/Source/App/DI/ShoppingSceneDIContainer.swift
@@ -9,6 +9,50 @@ import Foundation
 import UIKit
 
 class ShoppingDIContainer {
+    
+    lazy var naverapiconfig: NetworkConfiguration = {
+       let apiconfig = NetworkConfiguration(baseURL: URL(string: "https://openapi.naver.com/")!, header: ["X-Naver-Client-Id":APIKey.clientID,"X-Naver-Client-Secret":APIKey.clientSecret], queryParameter: ["display":30])
+        return apiconfig
+    }()
+    
+    lazy var realmRepository: BookmarkRepository = {
+        let realmBookmarkRepository = RealmBookmarkRepository()
+        return realmBookmarkRepository
+    }()
+    
+    lazy var naverNetworkService: NetworkService = {
+        let networkService = DefaultNetworkService(config: naverapiconfig)
+        
+        return networkService
+    }()
+    
+    lazy var dataTransferService: DataTransferService = {
+        let dataTransferService = DataTransferService(networkService: naverNetworkService)
+        
+        return dataTransferService
+    }()
+    
+    lazy var shoppingRepository: ShoppingRepository = {
+       let repository = DefaultShoppingRepository(dataTransferService: dataTransferService)
+        return repository
+    }()
+    
+    lazy var bookmarkRepository: BookmarkRepository = {
+        let repository = RealmBookmarkRepository()
+        return repository
+    }()
+    
+    lazy var searchProductUseCase: SearchProductUseCase = {
+        let usecase = DefaultSearchProductUseCase(shoppingRepository: shoppingRepository, bookmarkRepository: bookmarkRepository)
+        
+        return usecase
+    }()
+    
+    lazy var bookmarkUseCase: SetBookmarkUseCase = {
+        let usecase = RealmSetBookmarkUseCase(bookmark: bookmarkRepository)
+        return usecase
+    }()
+    
     func makeTabBarController(viewControllers: [UIViewController]) -> CustomTabBarViewController {
         let tabBarVC = CustomTabBarViewController()
         tabBarVC.setViewControllers(viewControllers, animated: false)
@@ -16,13 +60,13 @@ class ShoppingDIContainer {
     }
     
     func makeSearchViewController() -> SearchViewController {
-        let searchVC = SearchViewController()
+        let searchVC = SearchViewController(realmRepository: realmRepository, searchUseCase: searchProductUseCase, bookmarkUseCase: bookmarkUseCase)
         searchVC.tabBarItem = .init(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 0)
         return searchVC
     }
     
     func makeBookmarkViewController() -> BookmarkViewController {
-        let bookmarkVC = BookmarkViewController()
+        let bookmarkVC = BookmarkViewController(realmReposity: realmRepository)
         bookmarkVC.tabBarItem = .init(title: "좋아요", image: UIImage(systemName: "heart"), tag: 1)
         return bookmarkVC
     }
