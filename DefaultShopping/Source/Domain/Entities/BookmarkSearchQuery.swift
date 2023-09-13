ceb28bcfedc90f938efbc97665dfb5b58d004fd9
Kim junhwan <tazanboy1@naver.com>
2023-09-14 01:32:30 +0900

    [feat] 좋아요 검색 쿼리 구현

---
+++ DefaultShopping/Source/Domain/Entities/BookmarkSearchQuery.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarkSearchQuery.swift
+//  DefaultShopping
+//
+//  Created by JunHwan Kim on 2023/09/13.
+//
+
+import Foundation
+
+struct BookmarkSearchQuery {
+    let title: String
+    let page: Int
+}
